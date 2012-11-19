4d2bf36a2e403af4545706abb8436a5534abafef
Schuurdeur <info@vincentwebtech.nl>
2012-11-19 17:51:07 +0100

    First version of the plugin.
    TODO:
    Bug: if user got the stuff and getting removed from config.xml system still thinks he exists!
    Make configurable what items the users gets in the config.xml.

---
+++ src/main/java/com/github/schuurdeur/main.java
@@ -0,0 +1,77 @@
+package com.github.schuurdeur;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+
+final public class main extends JavaPlugin {
+      ArrayList<String> a = new ArrayList<String>();
+
+    @Override
+    public void onEnable() {
+        //config
+        this.saveDefaultConfig();
+
+        //new listener
+        getServer().getPluginManager().registerEvents(new Listener() {
+            @EventHandler
+            public void playerJoin(PlayerJoinEvent event) {
+               if(getConfig().getBoolean("enabled")) {
+                   //add current users from configuration files.
+                   if(!getConfig().getStringList("players").isEmpty()) {
+                       for (String players : getConfig().getStringList("players")) {
+                           a.add(players);
+                       }
+                   }
+
+                   //check users
+                   Player player = event.getPlayer();
+
+                   if(!getConfig().getStringList("players").isEmpty() && a.contains(player.getDisplayName())) {
+                       //if player is in the list
+                       player.sendMessage("Welcome back, " + player.getDisplayName() + "!");
+                   } else {
+                      player.sendMessage("Welcome to our server " + player.getDisplayName() + " !");
+                       //update inventory
+                       Inventory playerInventory = player.getInventory();
+
+                       //basic items for crafting
+                       playerInventory.addItem(new ItemStack(Material.WOOD_AXE, 1));
+                       playerInventory.addItem(new ItemStack(Material.WOOD_PICKAXE, 1));
+                       playerInventory.addItem(new ItemStack(Material.WOOD_SWORD, 1));
+                       playerInventory.addItem(new ItemStack(Material.WOOD_HOE,1));
+
+                       //basic armor
+                       playerInventory.addItem(new ItemStack(Material.LEATHER_BOOTS, 1));
+                       playerInventory.addItem(new ItemStack(Material.LEATHER_HELMET, 1));
+                       playerInventory.addItem(new ItemStack(Material.LEATHER_CHESTPLATE, 1));
+                       playerInventory.addItem(new ItemStack(Material.LEATHER_LEGGINGS, 1));
+
+                      //update the list.
+                      a.add(player.getDisplayName());
+                      getConfig().set("players", a.toString());
+                      saveConfig();
+                      reloadConfig();
+                      a.clear();
+                   }
+               }
+            }
+        }, this);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    //basic functions
+
+
+}

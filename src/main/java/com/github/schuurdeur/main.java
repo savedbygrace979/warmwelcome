c83cc5ff5d52f98baea9147cf69bd6852f2714dc
savedbygrace979 <adampruitt77@gmail.com>
2013-11-04 19:13:24 -0600

    Update main.java
    removed player array & username persistence in config.yml, implemented file checking for player.dat to check for new players. Added fetching from config for welcome message.

--- src/main/java/com/github/schuurdeur/main.java
+++ src/main/java/com/github/schuurdeur/main.java
@@ -2,6 +2,7 @@ package com.github.schuurdeur;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,64 +11,71 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
+import java.io.File;
 
 final public class main extends JavaPlugin {
-      ArrayList<String> a = new ArrayList<String>();
+      
 
+      
+    public void LoadDefaultConfig(){
+    	/** Method to load the default configuration and save it to the plugins directory 
+    	 * as config.yml when run for the first time */
+    	this.saveDefaultConfig();
+    }
+    
+    
+      
     @Override
     public void onEnable() {
         //config
-        this.saveDefaultConfig();
+        LoadDefaultConfig();
+    	
 
         //new listener
         getServer().getPluginManager().registerEvents(new Listener() {
-            @EventHandler
-            public void playerJoin(PlayerJoinEvent event) {
-               if(getConfig().getBoolean("enabled")) {
-                   //add current users from configuration files.
-                   if(!getConfig().getStringList("players").isEmpty()) {
-                       for (String players : getConfig().getStringList("players")) {
-                           a.add(players);
-                       }
-                   }
+            
+        	
+        	@EventHandler
+            public void onPlayerJoin(PlayerJoinEvent event) {
+            	Player player = event.getPlayer();
+            	String playername = player.getName();
+            	World world = player.getWorld();
+            	
+            	File playerdat = new File(world.getWorldFolder() + "\\players\\" + playername + ".dat");
+            	
+            	if(playerdat.exists()) {
+            		//Player has been here before, send them the welcome back message.
+            		player.sendMessage(ChatColor.RED + getConfig().getString("OldPlayer Welcome Prefix") + " " + player.getDisplayName() + " " + getConfig().getString("OldPlayer Welcome Suffix"));
+            	}
+            	
+            	else {
+            		//Player has never been here before, give them WarmWelcome and give some stuff.
+            		player.sendMessage(ChatColor.RED + getConfig().getString("NewPlayer Welcome Prefix") + " " + player.getDisplayName() +
+                            " " + getConfig().getString("NewPlayer Welcome Suffix"));
+            		
+            		//Log the New Player to the console & Log for admins & debug
+            		getLogger().info("Detected New Player - " + playername + " Invoking WarmWelcome Package");
+            		
+            	
+            		//update inventory
+            		Inventory playerInventory = player.getInventory();
 
-                   //check users
-                   Player player = event.getPlayer();
+            		//basic items for crafting
+            		playerInventory.addItem(new ItemStack(Material.WOOD_AXE, 1));
+            		playerInventory.addItem(new ItemStack(Material.WOOD_PICKAXE, 1));
+            		playerInventory.addItem(new ItemStack(Material.WOOD_SWORD, 1));
+            		playerInventory.addItem(new ItemStack(Material.WOOD_HOE,1));
+            		playerInventory.addItem(new ItemStack(Material.WOOD_SPADE,1));
 
-                   if(a.contains(player.getDisplayName())) {
-                       //if player is in the list
-                       player.sendMessage(ChatColor.RED + "[Warm Welcome] Welcome back, " + player.getDisplayName() + "!");
-                   } else {
-                      player.sendMessage(ChatColor.RED + "[Warm Welcome] Welcome to our server " + player.getDisplayName() +
-                                         " ! I hope you will enjoy my present.");
-                       //update inventory
-                       Inventory playerInventory = player.getInventory();
-
-                       //basic items for crafting
-                       playerInventory.addItem(new ItemStack(Material.WOOD_AXE, 1));
-                       playerInventory.addItem(new ItemStack(Material.WOOD_PICKAXE, 1));
-                       playerInventory.addItem(new ItemStack(Material.WOOD_SWORD, 1));
-                       playerInventory.addItem(new ItemStack(Material.WOOD_HOE,1));
-                       playerInventory.addItem(new ItemStack(Material.WOOD_SPADE,1));
-
-                       //basic armor
-                       playerInventory.addItem(new ItemStack(Material.LEATHER_BOOTS, 1));
-                       playerInventory.addItem(new ItemStack(Material.LEATHER_HELMET, 1));
-                       playerInventory.addItem(new ItemStack(Material.LEATHER_CHESTPLATE, 1));
-                       playerInventory.addItem(new ItemStack(Material.LEATHER_LEGGINGS, 1));
-
-                      //update the list.
-                       if(!a.contains(player.getDisplayName())) {
-                           a.add(player.getDisplayName());
-                       }
-                      getConfig().set("players", a.toArray());
-                      saveConfig();
-                      reloadConfig();
-                      a.clear();
-                   }
-               }
+            		//basic armor
+            		playerInventory.addItem(new ItemStack(Material.LEATHER_BOOTS, 1));
+            		playerInventory.addItem(new ItemStack(Material.LEATHER_HELMET, 1));
+            		playerInventory.addItem(new ItemStack(Material.LEATHER_CHESTPLATE, 1));
+            		playerInventory.addItem(new ItemStack(Material.LEATHER_LEGGINGS, 1));
+            	}
             }
+            
+            
         }, this);
     }
 
